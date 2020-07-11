79adf519f87670a92a90813d497ae8b03af63cb1
Ananth <31910950+ananth-24@users.noreply.github.com>
2020-07-11 13:54:45 +0530

    Add files via upload

---
+++ socialmedia-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCMvY9Vr0r6nH8nmUwJN0EX3potLNZn1kY",
+  authDomain: "socialmedia-fyp.firebaseapp.com",
+  databaseURL: "https://socialmedia-fyp.firebaseio.com",
+  projectId: "socialmedia-fyp",
+  storageBucket: "socialmedia-fyp.appspot.com",
+  messagingSenderId: "87760093727",
+  appId: "1:87760093727:web:2c18bca45aa51987760b2a",
+  measurementId: "G-LF4DP0XNXL"
+};
