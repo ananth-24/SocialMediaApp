503683ea42b203faa020d7703357532b5d224fc0
Ananth <31910950+ananth-24@users.noreply.github.com>
2020-07-11 14:00:55 +0530

    Add files via upload

---
+++ socialmedia-ui/build/precache-manifest.346003ab21a105857eabbc982f94da64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9785f343fe528f05e202210950f9714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7db65df85bee1dc6d00",
+    "url": "/static/css/main.b3f20669.chunk.css"
+  },
+  {
+    "revision": "5c64b6913a944845b43c",
+    "url": "/static/js/2.78241612.chunk.js"
+  },
+  {
+    "revision": "38a4b558739197dd9ec900c0561b7e3e",
+    "url": "/static/js/2.78241612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7db65df85bee1dc6d00",
+    "url": "/static/js/main.d24a0850.chunk.js"
+  },
+  {
+    "revision": "117815b9716db7833dbd",
+    "url": "/static/js/runtime-main.ad862155.js"
+  },
+  {
+    "revision": "601669e3d1cf3cd33d4966fb151411e4",
+    "url": "/static/media/icon.601669e3.png"
+  }
+]);
